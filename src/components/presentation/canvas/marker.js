73102089a570786686aa808e1ad2d63a41c44c10
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-04-13 14:43:14 +0900

    Create marker.js

---
+++ src/components/presentation/canvas/marker.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logger from 'utils/logger';
+import { buildStyle } from 'utils/builder';
+
+const Marker = ({
+  data,
+  style,
+}) => {
+  const {
+    path,
+    mask,
+    use,
+  } = data;
+
+  return (
+    <g style={style}>
+      <path
+        d={path._d}
+        style={buildStyle(path._style)}
+      />
+      <mask
+        id={mask._id}
+      >
+        <path
+          d={mask.path._d}
+          style={buildStyle(mask.path._style)}
+        />
+      </mask>
+      <use
+        mask={use._mask}
+        xlinkHref={use['_xlink:href']}
+      />
+    </g>
+        //href={buildFileURL(image['_xlink:href'])}
+  );
+};
+
+// Avoid re-render
+const areEqual = () => true;
+
+export default React.memo(Marker, areEqual);
