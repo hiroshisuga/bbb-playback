e503a6de9ca52cc2536399a18829984767f278ef
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-07-01 09:28:24 +0900

    more simple implementation
    revert the changes

--- src/utils/synchronizer.js
+++ src/utils/synchronizer.js
@@ -53,58 +53,25 @@ export default class Synchronizer {
 
     this.init();
   }
-/*
-  syncVolume() {
-    const volume = this.primary.volume();
-    const muted = this.primary.muted();
-
-    if (this.externalVideos) {
-      this.externalVideos.handleVolumeChange(volume,muted);
-    }
-  }
-*/
-   handleUpdateTime() {
-    const currentTime = this.primary.currentTime();
-
-    if (this.externalVideos && this.externalVideos.time !== currentTime) {
-      // only this one works, but volume, muted, rate are not tractable from here by this way..
-      this.externalVideos.time = currentTime;
-    }
-  }
 
   init() {
     STATUSES.forEach(status => {
       this.primary.on(status, () => this.status.primary = status);
-      if (this.secondary) {
-        this.secondary.on(status, () => this.status.secondary = status);
-      }
+      this.secondary.on(status, () => this.status.secondary = status);
     });
 
-    if (this.secondary) {
-      this.primary.on('play', () => this.secondary.play());
-      this.primary.on('pause', () => this.secondary.pause());
-    }
+    this.primary.on('play', () => this.secondary.play());
+    this.primary.on('pause', () => this.secondary.pause());
 
     this.primary.on('seeking', () => {
       const currentTime = this.primary.currentTime();
-      if (this.secondary) {
-        this.secondary.currentTime(currentTime);
-      }
+      this.secondary.currentTime(currentTime);
     });
 
     this.primary.on('ratechange', () => {
       const playbackRate = this.primary.playbackRate();
-      if (this.secondary) {
-        this.secondary.playbackRate(playbackRate);
-      }
+      this.secondary.playbackRate(playbackRate);
     });
-
-    //do this at external_video-player/index.js
-    // (not by passing props but by getting the values from primary player directly (dirty..)
-    //this.primary.on('volumechange', () => this.syncVolume());
-
-    // Actually this does not work at all..
-    //this.primary.on('timeupdate', () => this.handleUpdateTime());
 
     this.primary.on('waiting', () => {
       if (!this.synching && this.status.secondary === 'canplay') {
@@ -120,21 +87,19 @@ export default class Synchronizer {
       }
     });
 
-    if (this.secondary) {
-      this.secondary.on('waiting', () => {
-        if (!this.synching && this.status.primary === 'canplay') {
-          this.synching = true;
-         this.primary.pause();
-        }
-      });
+    jjjjthis.secondary.on('waiting', () => {
+      if (!this.synching && this.status.primary === 'canplay') {
+        this.synching = true;
+        this.primary.pause();
+      }
+    });
 
-      this.secondary.on('canplay', () => {
-        if (this.synching) {
-          this.synching = false;
-          this.primary.play();
-        }
-      });
-    }
+    this.secondary.on('canplay', () => {
+      if (this.synching) {
+        this.synching = false;
+        this.primary.play();
+      }
+    });
 
     // IMPORTANT: Blink holds the secondary media down while the document
     // page is not visible
