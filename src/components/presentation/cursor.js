dc68651f8a5938cc65c6edd92dcdecc0a4e97802
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-07-08 22:31:24 +0900

    Update cursor.js

--- src/components/presentation/cursor.js
+++ src/components/presentation/cursor.js
@@ -3,7 +3,7 @@ import { useCurrentIndex } from 'components/utils/hooks';
 import storage from 'utils/data/storage';
 import './index.scss';
 
-const getCursor = (index, viewBox) => {
+const getCursor = (index, thisCursor, viewBox) => {
   const inactive = {
     x: -1,
     y: -1,
@@ -11,7 +11,7 @@ const getCursor = (index, viewBox) => {
 
   if (index === -1) return inactive;
 
-  const currentData = storage.cursor[index];
+  const currentData = thisCursor[index];
   if (currentData.x === -1 && currentData.y === -1) return inactive;
 
   return {
@@ -20,18 +20,49 @@ const getCursor = (index, viewBox) => {
   };
 };
 
-const Cursor = ({ viewBox }) => {
-  const currentIndex = useCurrentIndex(storage.cursor);
-  const { x, y } = getCursor(currentIndex, viewBox);
+const Cursor = ({ viewBox, userId }) => {
+  const thisCursor = userId ? storage.cursors[userId] : storage.cursor;
+  const currentIndex = useCurrentIndex(thisCursor);
+  const { x, y } = getCursor(currentIndex, thisCursor, viewBox);
+
+  const currentPresentersIndex = useCurrentIndex(storage.presenters);
+  let presenterId = userId;
+  if (storage.presenters) {
+    const presenter =  storage.presenters[currentPresentersIndex];
+    presenterId = presenter ? presenter.userId : "none";
+  }
 
   if (x === -1 || y === -1) return null;
 
-  return (
-    <circle
-      className="cursor"
-      style={{ cx: x, cy: y }}
-    />
-  );
+  if (userId == presenterId) {
+    return (
+      <circle
+        className="cursor"
+        style={{ cx: x, cy: y }}
+      />
+    );
+  } else {
+    return (
+      <g>
+      <circle
+        className="cursor-nonpresenters"
+        style={{ cx: x, cy: y }}
+      />
+      { storage.participants ?
+        <text
+         className="cursor-label"
+          x={x}
+          y={y}
+          dx="0.6rem"
+          dy="0.3rem"
+        >
+          { storage.participants[userId].name }
+        </text>
+        : null
+      }
+      </g>
+    );
+  }
 };
 
 export default Cursor;
