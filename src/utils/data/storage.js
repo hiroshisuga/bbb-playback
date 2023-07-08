48eb3aada2592308bc1c11fab7bc0d881330c665
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2023-07-08 22:45:50 +0900

    Update storage.js

--- src/utils/data/storage.js
+++ src/utils/data/storage.js
@@ -196,6 +196,9 @@ const storage = {
   get cursor() {
     return DATA[ID.CURSOR];
   },
+  get cursors() {
+    return DATA[ID.CURSORS];
+  },
   get media() {
     return DATA[ID.MEDIA];
   },
@@ -218,6 +221,12 @@ const storage = {
   },
   get panzooms() {
     return DATA[ID.PANZOOMS];
+  },
+  get presenters() {
+    return DATA[ID.PRESENTERS];
+  },
+  get participants() {
+    return DATA[ID.PARTICIPANTS];
   },
   get screenshare() {
     return DATA[ID.SCREENSHARE];
